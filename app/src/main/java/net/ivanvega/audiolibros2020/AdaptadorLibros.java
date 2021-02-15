473a1c9cee2500d2421460293afe8c7fbc41c0a4
Giovanni Medina <grablens_war@hotmail.com>
2021-02-14 18:24:32 -0600

    Calando versionamiento a mi github

--- app/src/main/java/net/ivanvega/audiolibros2020/AdaptadorLibros.java
+++ app/src/main/java/net/ivanvega/audiolibros2020/AdaptadorLibros.java
@@ -14,6 +14,7 @@ import java.util.Vector;
 
 public class AdaptadorLibros extends  RecyclerView.Adapter<AdaptadorLibros.ViewHolder> {
     //calando si jala
+    String nada;
     private  Vector<Libro> vectorLibros;
     private  Context contexto;
     private  LayoutInflater inflador;
